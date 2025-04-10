8872c086214feac62531003c91ad4817c4ccb1b2
cristian.hernandez <cristian.hernandez@doublevpartners.com-at-960624215608>
2025-04-10 13:42:23 -0500

    feat: Refactor video sharing and loading states to use ValueNotifiers for improved state management

--- lib/video/video_with_interactions/parallax_video_card.dart
+++ lib/video/video_with_interactions/parallax_video_card.dart
@@ -8,7 +8,6 @@ import 'package:flutter_svg/svg.dart';
 import 'package:gap/gap.dart';
 import 'package:stoyco_shared/design/screen_size.dart';
 import 'package:stoyco_shared/envs/envs.dart';
-import 'package:stoyco_shared/utils/logger.dart';
 import 'package:stoyco_shared/video/models/video_info_with_user_interaction.dart';
 import 'package:stoyco_shared/video/video_with_interactions/video_cache_service.dart';
 import 'package:video_player/video_player.dart';
@@ -93,10 +92,10 @@ class _ParallaxVideoCardState extends State<ParallaxVideoCard> {
   late Future<void> _initializeVideoPlayerFuture;
   bool _isPlayerInitialized = false;
   final _videoCacheService = VideoCacheService();
+  final ValueNotifier<bool> _isSharing = ValueNotifier(false);// Add this variable to track sharing state
+  final ValueNotifier<bool> isMuted = ValueNotifier(false);
   double _currentTime = 0;
-  bool isMuted = false;
   bool isDisposed = false;
-  bool _isSharing = false; // Add this variable to track sharing state
   String? _cachedGifPath; // New variable for caching GIF path
 
   /// Gets the video base URL based on the current environment
@@ -105,7 +104,7 @@ class _ParallaxVideoCardState extends State<ParallaxVideoCard> {
   @override
   void initState() {
     super.initState();
-    isMuted = widget.isMuted;
+    isMuted.value = widget.isMuted;
     _initializeVideoPlayerFuture = _initializeController();
   }
 
@@ -124,8 +123,8 @@ class _ParallaxVideoCardState extends State<ParallaxVideoCard> {
 
       setState(() {
         _controller = controller;
+        _currentTime=0;
         _isPlayerInitialized = true;
-        _currentTime = 0;
       });
 
       _controller?.addListener(checkVideoCompletion);
@@ -187,11 +186,9 @@ class _ParallaxVideoCardState extends State<ParallaxVideoCard> {
   void toggleMute() {
     if (_controller == null) return;
 
-    setState(() {
-      isMuted = !isMuted;
-    });
-    _controller?.setVolume(isMuted ? 0 : 1);
-    widget.mute(isMuted);
+    isMuted.value = !isMuted.value;
+    _controller?.setVolume(isMuted.value ? 0 : 1);
+    widget.mute(isMuted.value);
   }
 
   Future<String> _loadGifFromAssets() async {
@@ -207,24 +204,7 @@ class _ParallaxVideoCardState extends State<ParallaxVideoCard> {
   }
 
   Future<void> _handleShare() async {
-    File? tempFile;
-    File? originalFile;
-
-    // Configuration variables for the logo with fallback values
-    final int logoWidth = widget.logoWidth ?? 100;
-    final int logoHeight = widget.logoHeight ?? 150;
-    final int overlayXOffset = widget.overlayX ?? 30;
-    final int overlayYOffset = widget.overlayY ?? 30;
-
-    // Build the position strings for FFmpeg overlay filter
-    final String overlayX = 'main_w-overlay_w-$overlayXOffset';
-    final String overlayY = 'main_h-overlay_h-$overlayYOffset';
-
-    // Build the FFmpeg filter using the variables
-    final String filterComplex =
-        '[1:v]scale=$logoWidth:$logoHeight[logo];[0:v][logo]overlay=$overlayX:$overlayY:shortest=1';
-
-    // Text to share
+    // Obtén la URL del video y el texto para compartir
     final video = widget.videoInfo.video;
     final videoUrl = (widget.videoInfo.video.appUrl ?? '');
     final shareText = widget.shareText ??
@@ -232,81 +212,29 @@ class _ParallaxVideoCardState extends State<ParallaxVideoCard> {
 Ver video: $videoUrl''';
 
     try {
-      setState(() {
-        _isSharing = true;
-      });
+      _isSharing.value = true;
+      if (videoUrl.isNotEmpty) {
+        await _controller?.pause();
 
-      // if (videoUrl.isNotEmpty) {
-      //   // Load animated GIF from assets and obtain the path
-      //   final gifPath = await _loadGifFromAssets();
-
-      //   final outputPath =
-      //       '${(await getTemporaryDirectory()).path}/${video.name?.replaceAll(' ', '_') ?? 'video${video.id}'}_with_watermark.mp4';
-      //   debugPrint('Output path: $outputPath');
-      //   tempFile = File(outputPath);
-
-      //   // Local function that executes a command and shares the video if successful
-      //   Future<bool> tryExecuteCommand(String command) async {
-      //     final session = await FFmpegKit.execute(command);
-      //     final returnCode = await session.getReturnCode();
-      //     if (ReturnCode.isSuccess(returnCode) && await tempFile!.exists()) {
-      //       await _controller?.pause();
-      //       await Share.shareXFiles([XFile(tempFile.path)], text: shareText);
-      //       return true;
-      //     }
-      //     return false;
-      //   }
-
-      //   // First attempt: using h264_mediacodec
-      //   final command1 =
-      //       '-i $videoUrl -stream_loop -1 -i $gifPath -filter_complex "$filterComplex" -c:v h264_mediacodec -c:a copy $outputPath';
-      //   if (!(await tryExecuteCommand(command1))) {
-      //     // Second attempt: using mpeg4 codec
-      //     final command2 =
-      //         '-i $videoUrl -stream_loop -1 -i $gifPath -filter_complex "$filterComplex" -c:v mpeg4 -q:v 3 -c:a copy $outputPath';
-      //     if (!(await tryExecuteCommand(command2))) {
-      //       // Third attempt: share original video without watermark
-      //       final tempOrigPath =
-      //           '${(await getTemporaryDirectory()).path}/original_${video.name?.replaceAll(' ', '_') ?? 'video${video.id}'}.mp4';
-      //       originalFile = File(tempOrigPath);
-      //       final result =
-      //           await FFmpegKit.execute('-i $videoUrl -c copy $tempOrigPath');
-      //       final origReturnCode = await result.getReturnCode();
-      //       if (ReturnCode.isSuccess(origReturnCode) &&
-      //           await originalFile.exists()) {
-      //         await Share.shareXFiles([XFile(originalFile.path)],
-      //             text: shareText,);
-      //       }
-      //     }
-      //   }
-      // }
+        await Share.share(shareText);
+      }
 
       if (widget.play) {
         await _controller?.play();
       }
+
       if (widget.onShare != null) {
         widget.onShare!();
       }
     } catch (e) {
+      // Manejo de errores
       if (widget.play) {
         await _controller?.play();
       }
       debugPrint('Error sharing video: $e');
     } finally {
-      try {
-        if (tempFile != null && await tempFile.exists()) {
-          await tempFile.delete();
-        }
-        if (originalFile != null && await originalFile.exists()) {
-          await originalFile.delete();
-        }
-      } catch (e) {
-        StoyCoLogger.error('Error deleting temporary files: $e');
-      }
       if (mounted && !isDisposed) {
-        setState(() {
-          _isSharing = false;
-        });
+        _isSharing.value = false;
       }
     }
   }
@@ -466,12 +394,15 @@ Ver video: $videoUrl''';
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.end,
                         children: [
-                          GestureDetector(
-                            onTap: toggleMute,
-                            child: Icon(
-                              isMuted ? Icons.volume_off : Icons.volume_up,
-                              color: Colors.white,
-                              size: 24,
+                          ValueListenableBuilder<bool>(
+                            valueListenable: isMuted,
+                            builder: (context, value, child) => GestureDetector(
+                              onTap: toggleMute,
+                              child: Icon(
+                                value ? Icons.volume_off : Icons.volume_up,
+                                color: Colors.white,
+                                size: 24,
+                              ),
                             ),
                           ),
                           Column(
@@ -544,33 +475,35 @@ Ver video: $videoUrl''';
                                 Row(
                                   mainAxisAlignment: MainAxisAlignment.center,
                                   children: [
-                                    InkWell(
-                                      onTap: _isSharing ? null : _handleShare,
-                                      child: _isSharing
-                                          ? SizedBox(
-                                              width: StoycoScreenSize.width(
-                                                context,
-                                                20,
-                                              ),
-                                              height: StoycoScreenSize.width(
-                                                context,
-                                                20,
-                                              ),
-                                              child:
-                                                  const CircularProgressIndicator(
-                                                strokeWidth: 2,
-                                                color: Colors.white,
-                                              ),
-                                            )
-                                          : SvgPicture.asset(
-                                              'packages/stoyco_shared/lib/assets/icons/share_outlined_icon.svg',
-                                              width: StoycoScreenSize.width(
-                                                context,
-                                                20,
-                                              ),
-                                              color: Colors.white,
-                                            ),
-                                    ),
+                                   InkWell(
+                                     onTap: _isSharing.value ? null : _handleShare,
+                                     child: ValueListenableBuilder<bool>(
+                                       valueListenable: _isSharing,
+                                       builder: (context, isSharing, child) => isSharing
+                                           ? SizedBox(
+                                               width: StoycoScreenSize.width(
+                                                 context,
+                                                 20,
+                                               ),
+                                               height: StoycoScreenSize.width(
+                                                 context,
+                                                 20,
+                                               ),
+                                               child: const CircularProgressIndicator(
+                                                 strokeWidth: 2,
+                                                 color: Colors.white,
+                                               ),
+                                             )
+                                           : SvgPicture.asset(
+                                               'packages/stoyco_shared/lib/assets/icons/share_outlined_icon.svg',
+                                               width: StoycoScreenSize.width(
+                                                 context,
+                                                 20,
+                                               ),
+                                               color: Colors.white,
+                                             ),
+                                     ),
+                                   ),
                                     Gap(StoycoScreenSize.width(context, 8.05)),
                                     Text(
                                       '${widget.videoInfo.video.videoMetadata?.shared ?? 0}',
