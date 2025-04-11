0b4030b268623036412c54d3889adab61bc9bd24
JSRamirezDev <sebastian@jsramirez.com>
2025-04-11 16:34:43 -0500

    feat: Add platform parameter to processAuthCode methods in TikTokDataSource, TikTokRepository, and TikTokService for enhanced functionality

--- lib/tiktok/tiktok_data_source.dart
+++ lib/tiktok/tiktok_data_source.dart
@@ -3,7 +3,6 @@ import 'package:dio/dio.dart';
 import 'package:stoyco_shared/envs/envs.dart';
 
 class TikTokDataSource {
-
   TikTokDataSource({required this.environment});
   final StoycoEnvironment environment;
   final Dio _dio = Dio();
@@ -12,11 +11,19 @@ class TikTokDataSource {
     required String code,
     required String userId,
     required String codeVerifier,
+    required String platform,
   }) async {
     final response = await _dio.post(
       environment.urlTikTok,
       options: Options(headers: {'Content-Type': 'application/json'}),
-      data: jsonEncode({'user_id': userId, 'code': code,'code_verifier':codeVerifier}),
+      data: jsonEncode(
+        {
+          'user_id': userId,
+          'code': code,
+          'code_verifier': codeVerifier,
+          'platform': platform
+        },
+      ),
     );
     return response;
   }
