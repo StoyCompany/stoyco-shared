168751521f2b80542d450d99f0f4d747b63fb45d
cristian <cristian.hernandez@doublevpartners.com-at-960624215608>
2025-09-02 16:40:39 -0500

    feat: Add user attribute and in-app messaging methods to MoEngage service
    This commit introduces new methods to the MoEngage service and its platform implementations:
    
    - `showInAppMessage()`: Displays in-app messages (no-op on Web).
    - `showNudge()`: Shows nudge notifications (no-op on Web).
    - `setUserName(String userName)`
    - `setUserEmail(String email)`
    - `setGender(MoEGender gender)` (with a helper `stringToGender` in `MoengageService`)
    - `setPhoneNumber(String phoneNumber)`
    - `setLastName(String lastName)`
    - `setFirstName(String firstName)`
    
    For the mobile platform (`moengage_mobile_platform.dart`):
    - In-app message callbacks (`_onInAppClick`, `_onInAppShown`, `_onInAppDismissed`, `_onInAppSelfHandled`) are now set up during initialization.
    - Logging has been added for these new methods.
    
    For the web platform (`moengage_web_platform.dart`):
    - The new user attribute methods are implemented.
    - `showInAppMessage` and `showNudge` are implemented as no-op methods with corresponding log messages.
    
    The `MoEngagePlatform` abstract class and `MoengageService` have been updated to include these new method definitions.
    
    (cherry picked from commit a6fbaa8aa38c431b23abbc49f3b4509135f970a8)

--- lib/moengage/moengage_platform.dart
+++ lib/moengage/moengage_platform.dart
@@ -1,3 +1,5 @@
+import 'package:moengage_flutter/moengage_flutter.dart';
+
 abstract class MoEngagePlatform {
   /// Initializes the MoEngage SDK with the specific App ID.
   Future<void> initialize({required String appId});
@@ -14,4 +16,25 @@ abstract class MoEngagePlatform {
 
   /// Logs out the current user from MoEngage.
   Future<void> logout();
+
+  ///show in app message if there is any campaign available.
+  ///Only works on Android and iOS.
+  Future<void> showInAppMessage();
+
+  ///show nudge if there is any campaign available.
+  ///Only works on Android and iOS.
+  Future<void> showNudge();
+
+  /// Sets the user's name.
+  Future<void> setUserName(String userName);
+  /// Sets the user's email.
+  Future<void> setUserEmail(String email);
+  /// Sets the user's gender.
+  Future<void> setGender(MoEGender gender);
+  /// Sets the user's phone number.
+  Future<void> setPhoneNumber(String phoneNumber);
+  /// Sets the user's last name.
+  Future<void> setLastName(String lastName);
+  /// Sets the user's first name.
+  Future<void> setFirstName(String firstName);
 }
