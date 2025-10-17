beecfac270e8c0f1260275047df9a02864ea24bb
cristian <cristian.hernandez@doublevpartners.com>
2025-10-17 13:53:42 -0500

    refactor: Change `getFullPhoneNumber` to return an empty string on failure
    This commit updates the `getFullPhoneNumber` utility function to return an empty string (`''`) instead of `'User NOT LOGGED'` in cases where the phone number cannot be constructed.
    
    This change applies when:
    - The `countryCode` or `phoneNumber` is null or empty.
    - The country code cannot be found.
    - The corresponding dial code is empty.
    
    (cherry picked from commit f77bececab6e9997eb9ef61ebe4a487b4945b016)

--- lib/utils/full_phone_number_by_country_code.dart
+++ lib/utils/full_phone_number_by_country_code.dart
@@ -2,15 +2,15 @@ import 'package:stoyco_shared/form/data/countries.dart';
 
 String getFullPhoneNumber(String? countryCode, String? phoneNumber) {
   if (countryCode == null || countryCode.isEmpty || phoneNumber == null || phoneNumber.isEmpty) {
-    return 'User NOT LOGGED';
+    return '';
   }
   try {
     final country = stoycoCountries.firstWhere((c) => c.code == countryCode);
     final dialCode = country.dialCode ?? '';
-    if (dialCode.isEmpty) return 'User NOT LOGGED';
+    if (dialCode.isEmpty) return '';
     return '+$dialCode$phoneNumber';
   } catch (_) {
-    return 'User NOT LOGGED';
+    return '';
   }
 }
 
