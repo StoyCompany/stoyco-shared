47c515c00b2da759c35ae780ea4164129d84e563
jesussantander-wq <jesus.santander@doublevpartners.com>
2025-11-14 11:57:43 -0500

    sahred like component

--- lib/design/colors.dart
+++ lib/design/colors.dart
@@ -30,4 +30,6 @@ class StoycoColors {
   static Color softWhite = ColorUtils.fromHex('F8F9FA');
 
   static Color shadowColor = ColorUtils.fromHex('494949');
+
+  static Color whiteLavander = ColorUtils.fromHex('F7F6FA');
 }
