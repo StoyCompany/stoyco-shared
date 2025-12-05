6132cce300a12f8b7af4a55e52609e79784723c4
cristian <cristian.hernandez@doublevpartners.com>
2025-12-05 16:00:30 -0500

    feat: add user service

--- lib/stoycoins/stoycoins_service.dart
+++ lib/stoycoins/stoycoins_service.dart
@@ -1,5 +1,7 @@
 import 'package:either_dart/either.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:stoyco_shared/envs/envs.dart';
+import 'package:stoyco_shared/errors/error_handling/failure/error.dart';
 import 'package:stoyco_shared/errors/error_handling/failure/failure.dart';
 import 'package:stoyco_shared/models/page_result/page_result.dart';
 import 'package:stoyco_shared/stoycoins/models/balance.dart';
@@ -8,14 +10,16 @@ import 'package:stoyco_shared/stoycoins/models/donate_result.dart';
 import 'package:stoyco_shared/stoycoins/models/transactions.dart';
 import 'package:stoyco_shared/stoycoins/stoycoins_data_source.dart';
 import 'package:stoyco_shared/stoycoins/stoycoins_repository.dart';
+import 'package:stoyco_shared/stoycoins/user_service.dart';
 
 /// Service for Stoycoins operations: balance, donation, and transactions.
 class StoycoinsService {
   /// Factory constructor for singleton instance.
-  factory StoycoinsService({required StoycoEnvironment environment}) =>
-      _instance ??= StoycoinsService._(environment: environment);
+  factory StoycoinsService({required StoycoEnvironment environment,  UserService? userService}) =>
+      _instance ??= StoycoinsService._(environment: environment, userService: userService);
 
-  StoycoinsService._({required this.environment}) {
+  StoycoinsService._({required this.environment,  UserService? userService})
+      : _userService = userService ?? UserService() {
     _dataSource = StoycoinsDataSource(environment: environment);
     _repository = StoycoinsRepository(dataSource: _dataSource!);
     _instance = this;
@@ -27,12 +31,25 @@ class StoycoinsService {
   StoycoEnvironment environment;
   StoycoinsDataSource? _dataSource;
   StoycoinsRepository? _repository;
+  UserService _userService;
 
   /// Fetches the user's Stoycoins balance.
   ///
+  /// If [userId] is null, uses the currently authenticated Firebase user.
   /// Returns an [Either] with [Failure] or [BalanceModel].
-  Future<Either<Failure, BalanceModel>> getBalance(String userId) =>
-      _repository!.getBalance(userId: userId);
+  Future<Either<Failure, BalanceModel>> getBalance([String? userId]) async {
+    final targetUserId = userId ?? _userService.getCurrentUserId();
+    final isAnonymous = !_userService.isUserAuthenticated();
+    if (isAnonymous) {
+      return Left(ErrorFailure.decode(StateError('User not authenticated')));
+    }
+    return _repository!.getBalance(userId: targetUserId);
+  }
+
+  /// Gets the balance for the currently authenticated Firebase user.
+  ///
+  /// Returns an [Either] with [Failure] or [BalanceModel].
+  Future<Either<Failure, BalanceModel>> getCurrentUserBalance() => getBalance();
 
   /// Sends a donation transaction.
   ///
@@ -57,4 +74,3 @@ class StoycoinsService {
     pageSize: pageSize,
   );
 }
-
