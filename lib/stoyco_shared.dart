c18841d6985a6ec67f95350a5a86602069a08321
cristian <cristian@MacBook-Pro-de-cristian.local>
2025-12-10 16:34:12 -0500

    increment stoy coins in radio

--- lib/stoyco_shared.dart
+++ lib/stoyco_shared.dart
@@ -25,4 +25,5 @@ export 'package:stoyco_shared/widgets/social_button.dart';
 export 'package:stoyco_shared/widgets/shared_like.dart';
 export 'package:stoyco_shared/widgets/loop_video_player.dart';
 export 'package:stoyco_shared/services/social_interaction_service.dart';
+export 'package:stoyco_shared/services/collective_fund_service.dart';
 export 'package:stoyco_shared/radio/radio.dart';
